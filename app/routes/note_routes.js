378a8458498a51ccf91eb4312106b92805a96de8
Nick Jalalizadeh <nick.jalalizadeh@gmail.com>
2018-11-07 20:10:14 -0800

    First Commit

---
+++ app/routes/note_routes.js
@@ -0,0 +1,55 @@
+var ObjectID = require('mongodb').ObjectID;
+
+module.exports = function(app, db) {
+    //Creating a Note
+    app.post('/notes', (req, res) => {
+        const note = { text: req.body.body, title: req.body.title };
+        db.collection('notes').insert(note, (err, result) => {
+            if (err) { 
+                res.send({ 'error': 'An error has occurred' }); 
+            } else {
+                res.send(result.ops[0]);
+            }
+        });
+    });
+
+    //Reading a Note
+    app.get('/notes/:id', (req, res) => {
+        const id = req.params.id;
+        const details = { '_id': new ObjectID(id) };
+        db.collection('notes').findOne(details, (err, item) => {
+            if (err) {
+                res.send({'error':'An error has occurred'});
+            } else {
+                res.send(item);
+            } 
+        });
+    });
+
+    //Updating a Note
+    app.put('/notes/:id', (req, res) => {
+        const id = req.params.id;
+        const details = { '_id': new ObjectID(id) };
+        const note = { text: req.body.body, title: req.body.title };
+        db.collection('notes').update(details, note, (err, result) => {
+            if (err) {
+                res.send({'error':'An error has occurred'});
+            } else {
+                res.send(note);
+            } 
+        });
+    });
+
+    //Deleting a Note
+    app.delete('/notes/:id', (req, res) => {
+        const id = req.params.id;
+        const details = { '_id': new ObjectID(id) };
+        db.collection('notes').remove(details, (err, item) => {
+            if (err) {
+                res.send({'error':'An error has occurred'});
+            } else {
+                res.send('Note ' + id + ' deleted!');
+            } 
+        });
+    });
+};
---
+++ config/db.js
@@ -0,0 +1,4 @@
+module.exports = {
+    url : "mongodb://nickjalalizadeh:nickpass1@ds045107.mlab.com:45107/njalalizadeh-notes",
+    name : "njalalizadeh-notes"
+  };
