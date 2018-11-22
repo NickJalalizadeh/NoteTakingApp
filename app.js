b3f17496b62462d361e6c6d07e785dd05b776c73
Nick Jalalizadeh <nick.jalalizadeh@gmail.com>
2018-11-21 17:54:50 -0800

    Improved upon CSS styles

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ $(document).ready(function() {
     var selectedCard = null;
 
     function clickCard() {
-        selectedCard = $(this);
+        selectedCard = $(this).parent();
+        console.log(selectedCard);
         const cardTitle = selectedCard.find(".card-title").html();
         const cardText = selectedCard.find(".card-text").html();
         const cardColor = selectedCard.find(".card-body").css("background-color");
@@ -26,9 +27,15 @@ $(document).ready(function() {
 
     function appendNewCard() {
         $.get('noteInvisible.html', function(data){
-            $('.cardRow').append(data); // Append invisible card
-            $('.card.d-none').on('click', clickCard);
+            $('.cardRow').prepend(data); // Append invisible card
+            $('.d-none .card-body').on('click', clickCard);
         });
+    }
+
+    for (let i = 1; i <= 8; i++) {
+        let button = $("#color0").clone();
+        button.attr("id", "color" + i);
+        $("#colorGroup").append(button);
     }
 
     // Load all notes
@@ -37,11 +44,11 @@ $(document).ready(function() {
             var card = $(data);
             
             $.each(notes, function(i, note) {
-                card.on('click', clickCard);
+                card.find(".card-body").on('click', clickCard);
                 card.find(".card-title").html(note.title);
                 card.find(".card-text").html(note.body);
                 card.find(".card-body").css("background-color", note.color);
-                card.attr("id", note._id);
+                card.find(".card").attr("id", note._id);
                 $('.cardRow').append(card);
                 card = card.clone();
             });
@@ -53,7 +60,7 @@ $(document).ready(function() {
     $("#newNoteBtn").on("click", function() {
         const colorBtns = $("#colorGroup .btn");
         const initialBtn = colorBtns.first();
-        const initialColor = initialBtn.css("border-color");
+        const initialColor = initialBtn.css("background-color");
         
         $("#createBtn").show();
         $("#saveBtn").hide();
@@ -75,12 +82,12 @@ $(document).ready(function() {
 
         $.post("notes/", {title: modalTitle, body: modalBody, color: modalColor}, function(data) {
             console.log(data);
-            const createdCard = $(".card.d-none");
+            const createdCard = $(".d-none .card");
             createdCard.find(".card-title").html(data.title); // Select invisible card
             createdCard.find(".card-text").html(data.body);
             createdCard.find(".card-body").css("background-color", data.color);
             createdCard.attr("id", data._id);
-            createdCard.removeClass("d-none"); // Make new card visible
+            createdCard.parent().removeClass("d-none"); // Make new card visible
         
             appendNewCard();
         });
@@ -89,7 +96,7 @@ $(document).ready(function() {
     // Click save
     $("#saveBtn").on("click", function() {
         if (selectedCard != null) {
-            const id = selectedCard.attr("id");
+            const id = selectedCard.attr("id"); 
             const modalTitle = $("#titleInput").val();
             const modalBody = $("#textInput").val();
             const modalColor = $(".modal-body").css("background-color");
@@ -117,14 +124,20 @@ $(document).ready(function() {
                 type: 'DELETE',
                 success: function(data) {
                     console.log(data);
-                    selectedCard.remove();
+                    selectedCard.parent().remove();
                 }
             });
         }
     });
     
+    // Click color button
     $("input[type=radio][name=colors]").change(function() {
-        const selectedColor = $(this).parent().css('border-color');
+        var selectedColor = $(this).parent().css('border-color');
+        
+        if ($(this).parent().attr("id") == "color0") {
+            selectedColor = "#FFF";
+        }
+        console.log(selectedColor);
         $('.modal-header, .modal-body').css('background-color', selectedColor);
     });
 });
