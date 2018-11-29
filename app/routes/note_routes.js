415865536d66c3dbcf0d9d660123d8c2329681ef
Nick Jalalizadeh <nick.jalalizadeh@gmail.com>
2018-11-29 10:33:16 -0800

    changed server destinations

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -11,7 +11,7 @@ module.exports = function(app, db) {
     };
 
     //Creating a Note
-    app.post('/notes', (req, res) => {
+    app.post('NoteTakingApp/notes', (req, res) => {
         // Validate request
         if(!req.body.title && !req.body.body) {
             return res.status(400).send({
@@ -32,8 +32,8 @@ module.exports = function(app, db) {
         });
     });
 
-    //Reading a Note
-    app.get('/notes/:id', (req, res) => {
+    //Reading one Note
+    app.get('NoteTakingApp/notes/:id', (req, res) => {
         const id = req.params.id;
         const details = { '_id': new ObjectID(id) };
         db.collection('notes').findOne(details, (err, result) => {
@@ -45,8 +45,8 @@ module.exports = function(app, db) {
         });
     });
 
-    //Read all Notes
-    app.get('/notes', (req, res) => {
+    //Reading all Notes
+    app.get('NoteTakingApp/notes', (req, res) => {
         db.collection('notes').find({}).toArray((err, result) => {
             if (err) {
                 res.send({error: err.message || 'An error has occurred'});
@@ -57,7 +57,7 @@ module.exports = function(app, db) {
     });
 
     //Updating a Note
-    app.put('/notes/:id', (req, res) => {
+    app.put('NoteTakingApp/notes/:id', (req, res) => {
         // Define Note
         const note = defineNote(req.body);
 
@@ -73,7 +73,7 @@ module.exports = function(app, db) {
     });
 
     //Deleting a Note
-    app.delete('/notes/:id', (req, res) => {
+    app.delete('NoteTakingApp/notes/:id', (req, res) => {
         const id = req.params.id;
         const details = { '_id': new ObjectID(id) };
         db.collection('notes').remove(details, (err) => {
